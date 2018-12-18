3e14f844bed5e010018a9c7b7ec4ac9fe934d132
BT_1N3_26 <mister.p730@gmail.com>
2018-12-18 14:45:55 -0500

    Fixing errors

--- src/Maps/Town_1.java
+++ src/Maps/Town_1.java
@@ -15,7 +15,13 @@ public class Town_1
    }
    public void makeMap(int x,int y)
    {
-      map = new Gravel[x][y];
+      for(int i = 0; i<map.length;i++)
+      {
+         for(int z = 0; z<map[i].length;z++)
+         {
+            map[i][z] = new Gravel(i,z);
+         }
+      }
    }
    public void print()
    {
